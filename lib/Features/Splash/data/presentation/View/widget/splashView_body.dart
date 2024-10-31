7dbffb541c7af0cb62ecc61a1eaa8afaf16f74c1
Mohamed <tantawymo2003@gmail.com>
2024-10-31 17:52:17 +0300

    change mode to dart & add text and create splashView

--- lib/Features/Splash/data/presentation/View/widget/splashView_body.dart
+++ lib/Features/Splash/data/presentation/View/widget/splashView_body.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:product/core/utils/assets.dart';
 
 class SplashviewBody extends StatelessWidget {
   const SplashviewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column();
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Image.asset(AssetsData.logo),
+     const   Text('Read free books',
+        textAlign: TextAlign.center,),
+      ],
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:product/Features/Splash/data/presentation/View/splashView.dart';
+import 'package:product/constants.dart';
 
 void main() {
   runApp(const Bookapp());
 }
+
 class Bookapp extends StatelessWidget {
   const Bookapp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const GetMaterialApp(
-      home: Splashview(),
+    return GetMaterialApp(
+      theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: kprimaycolor),
+      home: const Splashview(),
     );
   }
 }
